d4f26728492b11a8c5bea5ee736e7402afbbccfb
leo-i-farias <fariasleandroivan@gmail.com>
2022-09-05 02:33:04 -0300

    Agregado servidor con las 5 rutas a las vistas creadas

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>{
+    res.send("Estamos en la vista Home");
+})
+app.get('/detail-product', (req, res) =>{
+    res.send("Estamos en la vista Detail Product");
+})
+app.get('/cart', (req, res) =>{
+    res.send("Estamos en la vista Cart");
+})
+app.get('/log-in', (req, res) =>{
+    res.send("Estamos en la vista Log In");
+})
+app.get('/register', (req, res) =>{
+    res.send("Estamos en la vista Register");
+})
+
+app.listen(PORT, () => console.log("Servidor escuchando en el puerto", PORT));
