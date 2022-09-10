fdd1b8c3839c439de21da7c4482f7a3bcb1306a0
leo-i-farias <fariasleandroivan@gmail.com>
2022-09-09 22:29:51 -0300

    Agregado objeto path y motor EJS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     res.send("Estamos en la vista Home");
