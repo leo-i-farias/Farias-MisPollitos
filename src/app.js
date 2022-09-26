450b1eff5685b71cb0456d665daa339ef6ca3ad4
leo-i-farias <fariasleandroivan@gmail.com>
2022-09-26 03:26:40 -0300

    Vistas corregidas

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(require('./routes/route'));
 
