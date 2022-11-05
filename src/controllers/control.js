e5d4ce921a3296b90c5c455eaf0937af1863a7e0
leo-i-farias <fariasleandroivan@gmail.com>
2022-11-04 22:01:49 -0300

    Creados controladores para login, registro y ver productos

--- src/controllers/control.js
+++ src/controllers/control.js
@@ -1,26 +1,59 @@
-const renderHome = (req, res) =>{
-    return res.render('index');   
+const readProductsDataBase = () =>{
+    const products = JSON.parse(file.readFileSync(path.join(__dirname, '/database.json'), 'utf-8'));
+    return products;
 }
-const renderCart = (req, res) =>{
-    return res.render('cart');
+const readUsersDataBase = () =>{
+    const users = JSON.parse(file.readFileSync(path.join(__dirname, '/users.json'), 'utf-8'));
+    return users;
 }
-
-const renderDetailProduct = (req, res) =>{
-    return res.render('detail_product');
+const writeUsersDataBase = (data) =>{
+    file.writeFileSync(path.join(__dirname, '/users.json'), JSON.stringify(data));
 }
-
-const renderLogIn = (req, res) =>{
-    return res.render('login');
+const hashPassword = (password) =>{
+    const password_hashed = bcrypt.hashSync(password, 10);
+    return password_hashed;
 }
-
-const renderRegister = (req, res) =>{
-    return res.render('register');
+const validatePassword = (password, user) =>{
+    const value = bcrypt.compareSync(password, user.password);
+    return value;
 }
-
+const viewProduct = (req, res) =>{
+    const products = readProductsDataBase();
+    const product_id = req.params.id;
+    const product = products.filter(i => i.id == product_id);
+    const producto = product[0];
+    res.render('detail_product', {producto});
+}
+const login = (req, res) =>{
+    const users = readUsersDataBase();
+    const {email, password} = req.body;
+    const user_login = users[0];
+    const password_valid = validatePassword(password, user_login);
+    if(user_login.email == email && password_valid){
+        req.session.user_login = user_login;
+        res.redirect('/');
+    }
+}
+const logout = (req, res) =>{
+    req.session.destroy();
+    res.redirect('/login');
+}
+const newUser = (req, res) =>{
+    const users = readUsersDataBase();
+    const {name, last_name, email, date_birth, password} = req.body;
+    const password_hash = hashPassword(password);
+    const new_user = {
+        name: name,
+        last_name: last_name,
+        birth_date: date_birth,
+        password: password_hash
+    };
+    users.push(new_user);
+    writeUsersDataBase(users);
+}
 module.exports = {
-    renderHome,
-    renderCart,
-    renderDetailProduct,
-    renderLogIn,
-    renderRegister
+    viewProduct,
+    login,
+    logout,
+    newUser
 };
