96751811ecdfb5e9a15efd2d2d83aa1a058f56ee
leo-i-farias <fariasleandroivan@gmail.com>
2022-11-04 22:06:21 -0300

    Rutas actualizadas

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const Route = require('../controllers/control');
+const Route = require('../controllers/renders');
+const Controller = require('../controllers/control');
 const router = express.Router();
 
 router.get('/', Route.renderHome);
 
 router.get('/cart', Route.renderCart);
 
-router.get('/product-name', Route.renderDetailProduct);
+router.get('/product/:id', Controller.viewProduct);
 
 router.get('/log-in', Route.renderLogIn);
+router.post('/login', Controller.login);
+router.get('/logout', Controller.logout);
 
 router.get('/register', Route.renderRegister);
+router.post('/register', Controller.newUser);
 
 module.exports = router;
